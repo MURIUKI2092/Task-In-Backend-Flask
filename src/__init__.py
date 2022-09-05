124b9fe4a1ddea74f06d86d464d7afe50ad0a4a6
James Muriuki <muriukijames33@gmail.com>
2022-09-05 12:30:06 +0300

    set up flask app

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask,jsonify
+import os
+def create_app(test_config=None):
+
+    app=Flask(__name__,instance_relative_config=True)
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY")
+        )
+    else:
+        app.config.from_mapping(test_config)
+        @app.get("/hello")
+        def sayHello():
+            return jsonify({
+                "message":"Hello World"
+            })
+    return app
